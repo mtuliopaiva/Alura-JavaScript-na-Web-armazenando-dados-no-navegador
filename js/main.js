fe47b7a1482424afc642a1a911cc53061b95a1a3
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-09-22 17:11:59 -0300

    Projeto alura JS armazenando os dados no navegador - 5 - Remover itens

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ form.addEventListener('submit', (evento) => {
 
     // Verificando se existe o item no array
     const existe = itens.find(elemento => elemento.nome === nome.value);
-    console.log(existe);
+    // console.log(existe);
 
     // Criando um objeto
     const itemAtual = {
@@ -34,19 +34,24 @@ form.addEventListener('submit', (evento) => {
     // Criando uma condicional que verifica o id
     if(existe){
         itemAtual.id = existe.id;
-        console.log(existe.id);
+        // console.log(existe.id);
         
         atualizaElemento(itemAtual);
 
         // Para sobrescrever o conteudo no localStorage - basta pegar o itens na posicao existe id e
         // igualar ao itemAtual
-        itens[existe.id] = itemAtual;
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
         
     }
     else{
         // Caso o item nao exista, a id do item vale o tamanho
         // assim sendo incrementa sempre
-        itemAtual.id=itens.length;
+        // Como estamos alterando o tamanho dinamicamente, nao faz sentido 
+        // passarmos o id relacionado com o tamanho do Array. Para isso vamos criar uma
+        // condição (if/else). Nesse caso estamos utilizando um operador ternario (?)
+        // Se itens[itens.length - 1] for positivo (itens[itens.length - 1]).id + 1 e em caso
+        // negativo será zero
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
 
         // Atualiza minha lista
         criaElemento(itemAtual);
@@ -77,10 +82,10 @@ form.addEventListener('submit', (evento) => {
 function criaElemento(item){
     // Utlizando a API do document para criar uma LI
     // MODELO da LI - <li class="item"><strong>7</strong>Camisas</li>
-    const novoItem = document.createElement('li'); /*criando tag*/
-    novoItem.classList.add('item');/*adicionando classe*/
+    const novoItem = document.createElement("li"); /*criando tag*/
+    novoItem.classList.add("item");/*adicionando classe*/
 
-    const numeroItem = document.createElement('strong');/*criando tag*/
+    const numeroItem = document.createElement("strong");/*criando tag*/
     numeroItem.innerHTML = item.quantidade;
     // Colocando um data atributtes atraves do JS
     numeroItem.dataset.id = item.id;
@@ -93,11 +98,36 @@ function criaElemento(item){
     
    /*Da forma abaixo seria o correto*/ 
    novoItem.appendChild(numeroItem);
-   novoItem.innerHTML += item.nome; 
+   novoItem.innerHTML += item.nome;
+
+   novoItem.appendChild(botaoDeleta(item.id));
    lista.appendChild(novoItem);
 }
 function atualizaElemento(item){
     document.querySelector("[data-id = '"+item.id+"']").innerHTML = item.quantidade;
+}
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X";
+
+    elementoBotao.addEventListener("click", function(){
+        // console.log(this);
+        deletaElemento(this.parentNode, id);
+    })
+
+    return elementoBotao;
+}
+function deletaElemento(tag, id){
+    tag.remove();
+
+    // Agora precisamos remover um item do array e depois escrever no localStorage
+    // itens.splice("O que queremos remover com indice", 1)
+    // console.log(id);
+    // itens.fundIndex retorna o index de um elemento qualquer.
+    // passa o nosso elemeneto que sera igual ao nosso id e deleta
+    itens.splice(itens.findIndex(elemento => elemento.id === id),1);
+    console.log(itens);
+    localStorage.setItem("itens",JSON.stringify(itens));
 }
 /*
 Criando um array
@@ -197,6 +227,132 @@ Atualizando as quantidades no respectivo id
 Para sobrescrever o conteudo no localStorage - basta pegar o itens na posicao existe id e
 igualar ao itemAtual
 itens[existe.id] = itemAtual;
+*/
+
+/*
+Deletando um elemento
+- Criada uma função que cria o botao de deletar (X).
+function botaoDeleta() {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X";
+
+    // Não é possivel usar a arrow function aqui pois a arrow function nao carrega o this
+    // Pois precisamos saber quem foi clicado
+    elementoBotao.addEventListener("click", function(){
+        // console.log(this);
+        deletaElemento(this.parentNode);
+    })
+
+    return elementoBotao;
+}
+
+Criando um elemento no nosso item - botao X
+   novoItem.appendChild(botaoDeleta());
+
+    Criando elementos de botao via JSON, eles nao recebem o addEventListener na leitura da pagina
+    como nosso form, por isso e necessario criar outro event listener com evento de clique
+    Não é possivel usar a arrow function aqui pois a arrow function nao carrega o this
+    Pois precisamos saber quem foi clicado. O parent Node aqui indica que desejamos excluir o elemento
+    pai de button X
+
+    elementoBotao.addEventListener("click", function(){
+        // console.log(this);
+        deletaElemento(this.parentNode); 
+    })
+
+    return elementoBotao;
+}
+
+Com isso podemos eliminar o item no nossa aplicação
+function deletaElemento(tag){
+    tag.remove();
+}
+Porém o próximo passo agora é eliminar de nosso localStorage
+
+*/
+
+/*
+CODIGO GABARITO
+
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento(item) {
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+    novoItem.appendChild(numeroItem)
+    
+    novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
+
+    lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+}
 */
 
 
